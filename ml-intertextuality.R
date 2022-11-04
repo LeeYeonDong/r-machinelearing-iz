6ca41cb5e4689b6bbc8d0d2039024027a0a950ad
LeeYeonDong <LeeYeonDong@users.noreply.github.com>
2022-11-04 19:29:52 +0900

    Add files via upload

---
+++ ml-intertextuality.R
@@ -0,0 +1,7 @@
+source("prerequisites.R")
+source("twit.R")
+source("got.R") 
+source("tesserae.R")
+doc.got <- got.load.txt("D:/대학원/논문/상호텍스트성/quantitative-intertextuality-master/chapter3/data/masked/got/S03E09.txt")
+
+ls(doc.got)
